8182994c8feb598a5e5304aa8fec14c707649694
Nirbhay Jain <nirbhayjain2000@yahoo.in>
2020-03-16 18:37:09 +0530

    Author Routes

--- server.js
+++ server.js
@@ -7,15 +7,18 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false, limit: '10mb'}));
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL, {
+mongoose.connect('mongodb://localhost/mybrary', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
@@ -24,5 +27,6 @@ db.on('error', error => console.log(error));
 db.once('open', ()=> console.log('Conncected to Mongoose'));
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 app.listen(process.env.PORT || 3000);
